9a1d6a2a55770d85e58c78ef195e045278d7baf3
Aleksey Shapoval <alex@negativeimprint.com>
2019-10-23 11:10:12 -0400

    Xcode setup

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Aleksey Shapoval on 10/27/19.
+//  Copyright © 2019 VoVa LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
+
