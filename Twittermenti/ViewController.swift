1ff8710ea3f04c00e718e9fe22f00a61e5c98462
Aleksey Shapoval <alex@negativeimprint.com>
2019-10-24 16:18:05 -0400

    Branch that pulls api key from plist.  Not going to pursue this for now.

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -11,21 +11,50 @@ import SwifteriOS
 
 class ViewController: UIViewController {
     
+    struct MySettings: Codable {
+        var apiKey: String
+        var apiSecret: String
+    }
+    
+    let settingsURL: URL = Bundle.main.url(forResource: "Secrets", withExtension: "plist")!
+    var settings: MySettings?
+    
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
 
-    let swifter = Swifter(consumerKey: "AKeipMRVc3HE96dU8k4qPVeWZ", consumerSecret: "GZhlR6uYtKznyMi0wIRAPLnuZXBGgiYJgWQMFrCgDeZ8RAqhpt")
+    
+    var swifter : Swifter?
+    
+    //(consumerKey: "AKeipMRVc3HE96dU8k4qPVeWZ", consumerSecret: "GZhlR6uYtKznyMi0wIRAPLnuZXBGgiYJgWQMFrCgDeZ8RAqhpt")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        printApiInfo()
         
     }
 
     @IBAction func predictPressed(_ sender: Any) {
     
-    
     }
     
+    func printApiInfo () -> Swifter {
+        do {
+            let data = try Data(contentsOf: settingsURL)
+            let decoder = PropertyListDecoder()
+            settings = try decoder.decode(MySettings.self, from: data)
+            
+            swifter = Swifter(consumerKey: settings!.apiKey, consumerSecret: settings!.apiSecret)
+            
+            print(settings!.apiKey)
+            print(swifter)
+            
+        } catch {
+            // Handle error
+            print(error)
+        }
+        return swifter!
+    }
 }
 
